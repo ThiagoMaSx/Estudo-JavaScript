a330437fd6e0f6923d8dce1f9664542dc64c7bed
ThiagoMaSx <thiagowstf@gmail.com>
2026-09-16 17:27:56 -0300

    Aula sobre condicionais

---
+++ condicionais.js
@@ -0,0 +1,45 @@
+//const sum = 1 + 1;
+
+//if (sum === 2) {
+//    console.log("Sum is 2!");
+//} else if (sum === 3) {
+//     console.log("Sum is 3!");
+//} else if (sum === 4) {
+//     console.log("Sum is 4!");
+//} else if (sum === 5) {
+//    console.log("Sum is 5!")
+//} else {
+//    console.log("Sum is not 2, 3, 4 or 5.")
+//}
+
+//const sum1 = 2 + 2;
+//const sum2 = 3 + 3;
+
+//if (sum1 === 4 && sum2 === 6) {
+//    console.log("Sum is 4 and Sum2 is 6!")
+//}
+
+//if (sum1 === 4 || sum2 === 6){
+//    console.log("Sum is 4 or Sum2 is 6!")
+//    console.log(`Actual Sum values: Sum1 ${sum1}, Sum2: ${sum2} `)
+//}
+
+const sum = 1 + 3;
+
+let number = sum === 2 ? 2 : 4;
+
+console.log(number);
+
+let car = "Mercedes";
+
+switch(car) {
+    case "Mercedes":
+        console.log("Merces is a beautiful car!")
+        break;
+    case "Ferrari":
+        console.log("Ferrari its an fantastic car!")
+        break;
+    case "Tesla":
+        console.log("Tesla its a very smart car!")
+    
+}
